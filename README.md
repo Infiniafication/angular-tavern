549017ea040283380cc51d69f8f94529256ccad5
Infiniafication <isaachawks@gmail.com>
2019-12-21 04:59:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hfquxc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hfquxc)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Black Feather Tavern';
+  day = '21';
+  output = 'It is a sunny day.';
+  gold = 1000;
+  food = 500;
+  customerCount = 8; 
+}
