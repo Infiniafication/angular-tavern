47094fdf397926217906acd99761b740d6e67074
Infiniafication <isaachawks@gmail.com>
2020-01-02 19:34:39 +0800

    Refactor into Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HEROES } from './mock-heroes'; //TODO: Replace this with proper data storage method
+import { heroClass } from './hero-class';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,92 @@ export class HeroService {
   getHeroes(): Hero[] {
     return HEROES;
   }
+
+  getHeroClasses(): heroClass[] {
+    return heroClass;
+  }
+
+  // Determine hero's combat status based on current stats.
+  // Required: heroObject
+  calcStatus(heroObject) {
+    var statusFactor = 0;
+
+    if(heroObject.curHealth == 0)
+    {
+      heroObject.status = "Incapacitated";
+      return;
+    }
+    else if(heroObject.maxArcana > 0)
+    {
+      statusFactor = ( (heroObject.curArcana / heroObject.maxArcana) + (heroObject.curHealth / heroObject.maxHealth) + (1- heroObject.curFatigue / heroObject.maxFatigue) ) / 3;
+    }
+    else
+    {
+      statusFactor = ( (heroObject.curHealth / heroObject.maxHealth) + (1 - heroObject.curFatigue / heroObject.maxFatigue) ) / 2;
+    }
+
+    if (statusFactor >= 0.65) 
+    { 
+      heroObject.status = "Confident";
+      return;
+    }
+    else if(statusFactor >= 0.4) 
+    { 
+      heroObject.status = "Steady";
+      return;
+    }
+    else if(statusFactor > 0) 
+    { 
+      heroObject.status = "Weak";
+      return;
+    }
+    else 
+    { 
+      heroObject.status = "Incapacitated";
+      return;
+    }
+
+  }
+
+  // Initialize hero based on the Class
+  // Required: heroObject, heroClass
+  // Optional: spc, name
+  initHero(heroObject, heroClass, spc, name) {
+    if(name)
+    {
+      heroObject.name = name;
+    }
+
+    heroObject.heroClass = heroClass.name;
+    heroObject.maxHealth = heroClass.health;
+    heroObject.maxArcana = heroClass.arcana;
+    heroObject.maxFatigue = heroClass.fatigue;
+    heroObject.curHealth = heroClass.health;
+    heroObject.curArcana = heroClass.arcana;
+    heroObject.curFatigue = 0;
+
+    if(spc)
+    {
+      heroObject.spc = spc;
+      // TODO: Complete Specialization implementation
+      // heroObject.maxHealth = heroClass.health + heroSpc.health;
+      // heroObject.maxArcana = heroClass.arcana + heroSpc.arcana;
+      // heroObject.maxFatigue = heroClass.fatigue + heroSpc.fatigue;
+      // heroObject.curHealth = heroClass.health + heroSpc.health;
+      // heroObject.curArcana = heroClass.arcana + heroSpc.arcana;
+      // heroObject.curFatigue = 0;
+    }
+    else
+    {
+      heroObject.level = 1;
+    }
+
+    this.calcStatus(heroObject);
+  }
+  
+  // TODO: Implement selectClass()
+  // selectClass() {
+  //  show list of classes to choose
+  // }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,157 +9,33 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [
-    { 
-      id: 1, // 1 to 4
-      name: "Atlas",
-      level: 0, //1 to 30
-      heroClass: "",
-      spc: "", //Class Specialization
-      maxHealth: 0,
-      maxArcana: 0,
-      maxFatigue: 0,
-      curHealth: 0,
-      curArcana: 0,
-      curFatigue: 0,
-      status: "Incapacitated", // Incapacitated, Weak, Steady, Confident
-    },
-    {
-      id: 2, // 1 to 4
-      name: "Milo",
-      level: 0, //1 to 30
-      heroClass: "",
-      spc: "", //Class Specialization
-      maxHealth: 0,
-      maxArcana: 0,
-      maxFatigue: 0,
-      curHealth: 0,
-      curArcana: 0,
-      curFatigue: 0,
-      status: "Incapacitated", // Incapacitated, Weak, Steady, Confident
-    },
-    { 
-      id: 3, // 1 to 4
-      name: "",
-      level: 0, //1 to 30
-      heroClass: "",
-      spc: "", //Class Specialization
-      maxHealth: 0,
-      maxArcana: 0,
-      maxFatigue: 0,
-      curHealth: 0,
-      curArcana: 0,
-      curFatigue: 0,
-      status: "Incapacitated", // Incapacitated, Weak, Steady, Confident
-    },
-    {
-      id: 4, // 1 to 4
-      name: "",
-      level: 0, //1 to 30
-      heroClass: "",
-      spc: "", //Class Specialization
-      maxHealth: 0,
-      maxArcana: 0,
-      maxFatigue: 0,
-      curHealth: 0,
-      curArcana: 0,
-      curFatigue: 0,
-      status: "Incapacitated", // Incapacitated, Weak, Steady, Confident
-    }
-  ];
-
-  classes = heroClass;
-  
-  // TODO: Implement selectClass()
-  // selectClass() {
-  //  show list of classes to choose
-  // }
-
-  // Determine hero's combat status based on current stats.
-  // Required: heroObject
-  calcStatus(heroObject) {
-    var statusFactor = 0;
-
-    if(heroObject.curHealth == 0)
-    {
-      heroObject.status = "Incapacitated";
-      return;
-    }
-    else if(heroObject.maxArcana > 0)
-    {
-      statusFactor = ( (heroObject.curArcana / heroObject.maxArcana) + (heroObject.curHealth / heroObject.maxHealth) + (1- heroObject.curFatigue / heroObject.maxFatigue) ) / 3;
-    }
-    else
-    {
-      statusFactor = ( (heroObject.curHealth / heroObject.maxHealth) + (1 - heroObject.curFatigue / heroObject.maxFatigue) ) / 2;
-    }
-
-    if (statusFactor >= 0.65) 
-    { 
-      heroObject.status = "Confident";
-      return;
-    }
-    else if(statusFactor >= 0.4) 
-    { 
-      heroObject.status = "Steady";
-      return;
-    }
-    else if(statusFactor > 0) 
-    { 
-      heroObject.status = "Weak";
-      return;
-    }
-    else 
-    { 
-      heroObject.status = "Incapacitated";
-      return;
-    }
-
+  constructor(private heroService: HeroService) { }
+  ngOnInit() {
+    // TODO: Implement selectClass() functionality
+    this.getHeroes();
+    this.getHeroClasses();
+    // Mock hero init
+    this.initHero(this.heroes[0],this.classes[0],"",""); // Pass-in Fighter Hero Class
+    this.initHero(this.heroes[1],this.classes[2],"",""); // Pass-in Arcanist Hero Class
   }
 
+  // Variables
+  heroes: Hero[]
+  classes : heroClass[];
+
+  // Functions
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  getHeroClasses(): void {
+    this.classes = this.heroService.getHeroClasses();
+  }
+  
   // Initialize hero based on the Class
   // Required: heroObject, heroClass
   // Optional: spc, name
-  initHero(heroObject, heroClass, spc, name) {
-    if(name)
-    {
-      heroObject.name = name;
-    }
-
-    heroObject.heroClass = heroClass.name;
-    heroObject.maxHealth = heroClass.health;
-    heroObject.maxArcana = heroClass.arcana;
-    heroObject.maxFatigue = heroClass.fatigue;
-    heroObject.curHealth = heroClass.health;
-    heroObject.curArcana = heroClass.arcana;
-    heroObject.curFatigue = 0;
-
-    if(spc)
-    {
-      heroObject.spc = spc;
-      // TODO: Complete Specialization implementation
-      // heroObject.maxHealth = heroClass.health + heroSpc.health;
-      // heroObject.maxArcana = heroClass.arcana + heroSpc.arcana;
-      // heroObject.maxFatigue = heroClass.fatigue + heroSpc.fatigue;
-      // heroObject.curHealth = heroClass.health + heroSpc.health;
-      // heroObject.curArcana = heroClass.arcana + heroSpc.arcana;
-      // heroObject.curFatigue = 0;
-    }
-    else
-    {
-      heroObject.level = 1;
-    }
-
-    this.calcStatus(heroObject);
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-    // TODO: Implement selectClass() functionality
-    // Static hero init
-    this.initHero(this.heroes[0],this.classes[0],"",""); // Pass-in Fighter
-    this.initHero(this.heroes[1],this.classes[2],"",""); // Pass-in Arcanist
+  initHero(heroObject, heroClass, spc, name): void {
+    this.heroService.initHero(heroObject, heroClass, spc, name);
   }
 
 }
